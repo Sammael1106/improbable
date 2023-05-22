04d6368608033d64f16c4b37da3b0878478adea9
Darya <sammael1106@yandex.ru>
2023-05-22 16:55:54 +0200

    packages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@acme/ui', 'lodash-es'],
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  }
 }
 
 module.exports = nextConfig
