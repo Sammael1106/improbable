ecd7209dff27e9c2e6bff2f923a651fdf75e89e7
Darya <sammael1106@yandex.ru>
2023-05-22 17:07:05 +0200

    empty app

--- app/page.js
+++ app/page.js
@@ -1,95 +1,61 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+
+import styles from './improbable.module.css'
+import { clsx } from 'clsx';
+
+import React, { useRef, useEffect, useState } from 'react'
+import * as THREE from 'three';
+import { Leva, useControls } from 'leva'
+import { RGBELoader } from 'three-stdlib'
+import { Canvas, useLoader, useFrame, useThree } from '@react-three/fiber'
+import {
+  Center,
+  Html,
+  Float,
+  useGLTF,
+  Plane,
+  useCursor,
+  Instances,
+  Environment,
+  Lightformer,
+  OrbitControls,
+  RandomizedLight,
+  AccumulativeShadows,
+  MeshTransmissionMaterial,
+  ScrollControls,
+  Scroll,
+  useScroll,
+  Image
+} from '@react-three/drei'
+import gsap from 'gsap'
 
 export default function Home() {
+  const config = useControls({
+    zoom: { value: 65, min: 0, max: 100, step: 1 },
+    backside: true,
+    backsideThickness: { value: 0.3, min: 0, max: 2 },
+    samples: { value: 16, min: 1, max: 32, step: 1 },
+    resolution: { value: 1024, min: 64, max: 2048, step: 64 },
+    transmission: { value: 1, min: 0, max: 1 },
+    clearcoat: { value: 0, min: 0.1, max: 1 },
+    clearcoatRoughness: { value: 0.0, min: 0, max: 1 },
+    thickness: { value: 0.3, min: 0, max: 5 },
+    chromaticAberration: { value: 5, min: 0, max: 5 },
+    anisotropy: { value: 0.9, min: 0, max: 1, step: 0.01 },
+    roughness: { value: 0, min: 0, max: 1, step: 0.01 },
+    distortion: { value: 1, min: 0, max: 4, step: 0.01 },
+    distortionScale: { value: 1, min: 0.01, max: 1, step: 0.01 },
+    temporalDistortion: { value: .2, min: 0, max: 1, step: 0.01 },
+    ior: { value: 0, min: 0, max: 2, step: 0.01 },
+    scale: { value: 3, min: 0, max: 50, step: 0.01 },
+    color: '#ff9cf5',
+    gColor: '#ff7eb3',
+    shadow: '#750d57',
+  })
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <>
+     App here
+    </>
   )
 }
