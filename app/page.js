613cd0a5a25250af5e9dd16ab3ece1bc5a3566dd
Darya <sammael1106@yandex.ru>
2023-05-23 20:30:29 +0200

    responsive version

--- app/page.js
+++ app/page.js
@@ -28,29 +28,62 @@ import {
 } from '@react-three/drei'
 import gsap from 'gsap'
 
+const windowWidth = (typeof window === 'undefined') ? 1200 : window.innerWidth
+const windowHeight = (typeof window === 'undefined') ? 800 : window.innerHeight
+const viewRatio = windowHeight/windowWidth
+const viewVertical = viewRatio > 1
+let zoom = 65
+if (windowWidth <= 1200) zoom = 60
+if (windowWidth <= 1024) zoom = 57
+if (windowWidth <= 900) zoom = viewVertical ? 75 : 35
+if (windowWidth <= 500) zoom = 45
+
+console.log(zoom, windowWidth, windowHeight, viewVertical)
 export default function Home() {
-  const config = useControls({
-    zoom: { value: 65, min: 0, max: 100, step: 1 },
+  const config = {
+    zoom: zoom, // 65
     backside: true,
-    backsideThickness: { value: 0.3, min: 0, max: 2 },
-    samples: { value: 16, min: 1, max: 32, step: 1 },
-    resolution: { value: 1024, min: 64, max: 2048, step: 64 },
-    transmission: { value: 1, min: 0, max: 1 },
-    clearcoat: { value: 0, min: 0.1, max: 1 },
-    clearcoatRoughness: { value: 0.0, min: 0, max: 1 },
-    thickness: { value: 0.3, min: 0, max: 5 },
-    chromaticAberration: { value: 5, min: 0, max: 5 },
-    anisotropy: { value: 0.9, min: 0, max: 1, step: 0.01 },
-    roughness: { value: 0, min: 0, max: 1, step: 0.01 },
-    distortion: { value: 1, min: 0, max: 4, step: 0.01 },
-    distortionScale: { value: 1, min: 0.01, max: 1, step: 0.01 },
-    temporalDistortion: { value: .2, min: 0, max: 1, step: 0.01 },
-    ior: { value: 0, min: 0, max: 2, step: 0.01 },
-    scale: { value: 3, min: 0, max: 50, step: 0.01 },
+    backsideThickness: 0.3,
+    samples: 16,
+    resolution: 1024,
+    transmission: 1,
+    clearcoat: 0,
+    clearcoatRoughness: 0.0,
+    thickness: 0.3,
+    chromaticAberration: 5,
+    anisotropy: 0.9,
+    roughness: 0,
+    distortion: 1,
+    distortionScale: 1,
+    temporalDistortion: .2,
+    ior: 0,
+    scale: 3,
     color: '#ff9cf5',
     gColor: '#ff7eb3',
     shadow: '#750d57',
-  })
+  }
+  // const config = useControls({
+  //   zoom: { value: 65, min: 0, max: 100, step: 1 },
+  //   backside: true,
+  //   backsideThickness: { value: 0.3, min: 0, max: 2 },
+  //   samples: { value: 16, min: 1, max: 32, step: 1 },
+  //   resolution: { value: 1024, min: 64, max: 2048, step: 64 },
+  //   transmission: { value: 1, min: 0, max: 1 },
+  //   clearcoat: { value: 0, min: 0.1, max: 1 },
+  //   clearcoatRoughness: { value: 0.0, min: 0, max: 1 },
+  //   thickness: { value: 0.3, min: 0, max: 5 },
+  //   chromaticAberration: { value: 5, min: 0, max: 5 },
+  //   anisotropy: { value: 0.9, min: 0, max: 1, step: 0.01 },
+  //   roughness: { value: 0, min: 0, max: 1, step: 0.01 },
+  //   distortion: { value: 1, min: 0, max: 4, step: 0.01 },
+  //   distortionScale: { value: 1, min: 0.01, max: 1, step: 0.01 },
+  //   temporalDistortion: { value: .2, min: 0, max: 1, step: 0.01 },
+  //   ior: { value: 0, min: 0, max: 2, step: 0.01 },
+  //   scale: { value: 3, min: 0, max: 50, step: 0.01 },
+  //   color: '#ff9cf5',
+  //   gColor: '#ff7eb3',
+  //   shadow: '#750d57',
+  // })
 
   const [stage, setStage] = useState(1)
 
@@ -69,7 +102,7 @@ export default function Home() {
     })
   }, [stage])
 
-  const changeStage = (newstage) => {
+  const changeStage = (newstage, camera) => {
     console.log(overlay.current)
     overlay.current.classList = []
     overlay.current.classList.add(`from-stage${stage}-to-stage${newstage}`)
@@ -77,21 +110,17 @@ export default function Home() {
       opacity: 1,
       duration: 0.3,
       ease: "power3.out",
-      onComplete: () => setStage(newstage)
+      onComplete: () => {
+        camera.zoom = config.zoom;
+        camera.updateProjectionMatrix();
+        setStage(newstage)
+      }
     })
-  }
-
-  const stageClass = {
-    [styles.stage1]: stage === 1,
-    [styles.stage2]: stage === 2,
-    [styles.stage3]: stage === 3,
-    [styles.stage4]: stage === 4,
-    [styles.stage5]: stage === 5,
   }
 
   return (
     <>
-      <div  className={clsx(stageClass, styles.page)}>
+      <div  className={clsx(styles.page, styles[`stage${stage}`], styles[`zoom${config.zoom}`])}>
         <Canvas shadows orthographic camera={{ position: [0, 20, 0], zoom: config.zoom, near: 0, far: 1000 }} gl={{ preserveDrawingBuffer: true }}>
 
           {stage === 1 &&
@@ -117,7 +146,7 @@ export default function Home() {
         </Canvas>
       </div>
       <div ref={overlay} id={styles.overlay}></div>
-      <div className={clsx(stageClass, styles.pageIndicator)}>
+      <div className={clsx(styles[`stage${stage}`], styles.pageIndicator)}>
         <div className={styles.pageIndicatorProgress} style={{width: `${stage*20}%`}}></div>
       </div>
     </>
@@ -133,9 +162,6 @@ const StageOne = ({ config, setStage }) => {
   const materialPoperties = config;
   const iorLimits = [0.6, 0]
   useCursor(hovered)
-
-  // camera.zoom = config.zoom;
-  // camera.updateProjectionMatrix();
 
   useEffect(() => {
     gsap.to(material.current, {
@@ -153,7 +179,7 @@ const StageOne = ({ config, setStage }) => {
           camera.updateProjectionMatrix();
         },
         onComplete: function() {
-          setStage(2)
+          setStage(2, camera)
         }
       });
     }
@@ -164,13 +190,13 @@ const StageOne = ({ config, setStage }) => {
       <color attach="background" args={['#f2f2f5']} />
       <Grid />
 
-      <Html position={[-6, 0, -6]} className={styles.cell} distanceFactor={0.01555}>
+      <Html position={viewVertical ? [-3, 0, -6] : [-6, 0, -6]} className={viewVertical ? styles.doubleCell : styles.cell} distanceFactor={0.01555}>
         <h1 className={`${styles.heading} ${styles.alignRight} ${styles.alignBottom}`}><Heading>I'm Ready for the <span className={styles.emph}>Metaverse</span></Heading></h1>
       </Html>
 
-      <Html position={[3, 0, 0]} className={styles.doubleCell} distanceFactor={0.01555}>
+      <Html position={viewVertical ? [-3, 0, 3] : [3, 0, 0]} className={styles.doubleCell} distanceFactor={0.01555}>
         <p>Welcome! I'm <span className={styles.emph}>Daria Yudina</span>, a Technical Art Generalist with a passion for virtual worlds and an extensive background in web development, visual design, and 3D artistry.</p>
-        <div className={styles.arrowLeft} onClick={() => setClicked(true)}><span>let's dive deeper</span></div>
+        <div className={viewVertical ? styles.arrowRight : styles.arrowLeft} onClick={() => setClicked(true)}><span>let's dive deeper</span></div>
       </Html>
 
       <group position={[0,0.3,0]} scale={[3, 3, 3]} dispose={null}>
@@ -192,16 +218,12 @@ const StageOne = ({ config, setStage }) => {
 }
 
 const StageTwo = ({ config, setStage, stage }) => {
-  const { camera, viewport, scene } = useThree()
-
-
+  const { camera, viewport } = useThree()
   const materialPoperties = config;
   materialPoperties.ior = 0.5;
   materialPoperties.color = new THREE.Color('#46e3cb')
 
   useEffect(() => {
-    camera.zoom = config.zoom;
-    camera.updateProjectionMatrix();
     const scrollElement = document.getElementsByTagName('canvas')[0].nextSibling
     if (stage === 3) {
       scrollElement.scrollTo({ top: 2000, left: 0, behavior: 'smooth' })
@@ -212,28 +234,24 @@ const StageTwo = ({ config, setStage, stage }) => {
   })
 
   return (
-    <ScrollControls pages={2}>
+    <ScrollControls pages={1}>
       <color attach="background" args={['#000']} />
       <ScrollableGroups materialPoperties={materialPoperties}/>
-
-      <Html position={[-9, 0, -3]} className={styles.trippleCell}>
+      {/* <Grid /> */}
+      <Html position={viewVertical ? [-3, 0, -1]:[-9, 0, -2]} className={styles.trippleCell}>
         <h2 className={`${styles.heading} ${styles.alignBottom}`}><Heading>Born in the Digital Realm</Heading></h2>
-      </Html>
-      <Html position={[-9, 0, 0]} className={styles.trippleCell} >
-        <p  style={{marginTop: 16}}>I discovered my passion for digital worlds at an early age, a passion that led me to a career in web development and visual design. Today, I use my skills to create immersive experiences, believing that these worlds can provide us with a sense of fulfillment and endless opportunities for creativity.</p>
+        <p  style={{marginTop: 32}}>I discovered my passion for digital worlds at an early age, a passion that led me to a career in web development and visual design. Today, I use my skills to create immersive experiences, believing that these worlds can provide us with a sense of fulfillment and endless opportunities for creativity.</p>
         <div className={styles.row}>
-          <div className={`${styles.arrowLeft} ${styles.inactive}`} onClick={() => setStage(1)}><span>Back</span></div>
-          <div className={styles.arrowRight} onClick={() => setStage(3)}><span>Next</span></div>
+          <div className={`${styles.arrowLeft} ${styles.inactive}`} onClick={() => setStage(1, camera)}><span>Back</span></div>
+          <div className={styles.arrowRight} onClick={() => setStage(3, camera)}><span>Next</span></div>
         </div>
       </Html>
-      <Html position={[-1, 0, 20]} className={styles.trippleCell}>
+      <Html position={viewVertical ? [-3, 0, viewport.height - 6 ] : [0, 0, viewport.height - 3]} className={styles.trippleCell}>
         <h2 className={`${styles.heading} ${styles.alignBottom} ${styles.alignRight}`}>Skills and Expertise to Navigate the Metaverse</h2>
-      </Html>
-      <Html position={[-1, 0, 24]} className={styles.trippleCell}>
-        <p style={{marginTop: 16}} className={`${styles.alignRight}`}>Throughout my career, I've honed a diverse set of skills, from JavaScript to Unreal Engine, to bring my visions to life. I specialize in optimizing 2D & 3D assets, implementing them into game engines, and designing unique user interfaces.</p>
+        <p style={{marginTop: 32}} className={`${styles.alignRight}`}>Throughout my career, I've honed a diverse set of skills, from JavaScript to Unreal Engine, to bring my visions to life. I specialize in optimizing 2D & 3D assets, implementing them into game engines, and designing unique user interfaces.</p>
         <div className={styles.row} >
-          <div className={`${styles.arrowLeft} ${styles.inactive}`} style={{marginLeft: 'auto'}} onClick={() => setStage(2)}><span>Back</span></div>
-          <div className={styles.arrowRight} onClick={() => setStage(4)}><span>Next</span></div>
+          <div className={`${styles.arrowLeft} ${styles.inactive}`} style={{marginLeft: 'auto'}} onClick={() => setStage(2, camera)}><span>Back</span></div>
+          <div className={styles.arrowRight} onClick={() => setStage(4, camera)}><span>Next</span></div>
         </div>
       </Html>
     </ScrollControls>
@@ -266,7 +284,7 @@ const ScrollableGroups = ({materialPoperties}) => {
 
   return (
     <>
-      <group position={[0,0,0]} ref={floatingGroup}>
+      <group position={viewVertical ? [-3,0,-2] : [0,0,0]} ref={floatingGroup}>
         <Float {...floatProps} floatIntensity={Math.random() * 2}>
           <mesh scale={3} position={[3, -3, -2]}>
             <dodecahedronGeometry args={[0.5, 0]} />
@@ -299,9 +317,9 @@ const ScrollableGroups = ({materialPoperties}) => {
         </Float>
       </group>
 
-      <group ref={imagesGroup}>
-        <Image url="images/bug.png" rotation={[Math.PI/-2, 0, 0]} scale={8}  position={[-4, -5, viewport.height/2]} transparent={true} opacity={0}/>
-        <Image url="images/vscode.png" rotation={[Math.PI/-3, 0, 1]} scale={10} position={[-8, -8, viewport.height/-2 + 3]} transparent={true} opacity={0}/>
+      <group position={viewVertical ? [3,0,3] : [0,0,0]} ref={imagesGroup}>
+        <Image url="images/bug.png" rotation={[Math.PI/-2, 0, 0]} scale={8}  position={[-3, -5, viewport.height/2]} transparent={true} opacity={0}/>
+        <Image url="images/vscode.png" rotation={[Math.PI/-3, 0, 1]} scale={10} position={[-6, -8, viewport.height/-2 + 3]} transparent={true} opacity={0}/>
       </group>
     </>
   )
@@ -324,9 +342,6 @@ function StageFour({ config, setStage }) {
   const materialPoperties = config;
   materialPoperties.ior = 0
 
-  // camera.zoom = config.zoom;
-  // camera.updateProjectionMatrix();
-
   useFrame(() => {
     gsap.to(material.current, {
       ior: 0.6,
@@ -342,29 +357,28 @@ function StageFour({ config, setStage }) {
 
   return (
     <group dispose={null}>
-      <Html position={[-6.3, 0, -6]} className={styles.cell}>
+      <Html position={viewVertical ? [-3, 0, -8] : [-6, 0, -6]} className={viewVertical ? styles.doubleCell : styles.cell} style={{marginLeft: viewVertical ? 0 : -40}}>
         <h1 className={`${styles.heading} ${styles.alignRight} ${styles.alignBottom}`}>Why Improbable?</h1>
       </Html>
-
-      <Html position={[3, 0, -3.4]} className={styles.doubleCell}>
+      <Html position={viewVertical ? [-3, 0, 1] : [3, 0, -3.3]} className={styles.doubleCell} >
         <h3 className={`${styles.heading} ${styles.alignBottom}`}><Heading>An Alignment of Visions</Heading></h3>
-      </Html>
-      <Html position={[3, 0, -0.4]} className={styles.doubleCell}>
-        <p>Improbable's commitment to developing deeply immersive, interconnected virtual worlds resonates with my own beliefs. Your pioneering work in the field, specifically the Morpheus technology, inspires me, and I'm excited to potentially contribute to your efforts.</p>
+        <p style={{marginTop: 32}}>Improbable's commitment to developing deeply immersive, interconnected virtual worlds resonates with my own beliefs. Your pioneering work in the field, specifically the Morpheus technology, inspires me, and I'm excited to potentially contribute to your efforts.</p>
         <div className={styles.row} >
-          <div className={`${styles.arrowLeft} ${styles.inactive}`} onClick={() => setStage(3)}><span>Back</span></div>
-          <div className={styles.arrowRight} onClick={() => setStage(5)}><span>Next</span></div>
+          <div className={`${styles.arrowLeft} ${styles.inactive}`} onClick={() => setStage(3, camera)}><span>Back</span></div>
+          <div className={styles.arrowRight} onClick={() => setStage(5, camera)}><span>Next</span></div>
         </div>
       </Html>
       <Center>
         <mesh
+          position={viewVertical ? [0,0,-2] : [0,0,0]}
           scale={3}
           castShadow
           geometry={nodes.Cylinder.geometry}
+          onClick={() => setStage(5, camera)}
           >
             <meshBasicMaterial color={'white'} />
         </mesh>
-        <mesh scale={40} rotation={[Math.PI/-3,0,0]} position={[0, -3, 0]} ref={background}>
+        <mesh scale={40} rotation={[Math.PI/-3,0,0]} position={[0, -5, 0]} ref={background}>
           <planeGeometry />
           <MeshTransmissionMaterial {...config} ref={material} />
         </mesh>
@@ -379,20 +393,16 @@ function StageFour({ config, setStage }) {
 
 const StageFive = ({ config, setStage, stage }) => {
   const { camera } = useThree()
-  // camera.zoom = config.zoom;
-  // camera.updateProjectionMatrix();
 
   return (
     <>
       <color attach="background" args={['#000']} />
-
-      <Html position={[-9, 0, -4]} className={styles.trippleCell}>
+      {/* <Grid /> */}
+      <Html position={viewVertical ? [-3, 0, -3]:[-9, 0, -2]} className={styles.trippleCell} distanceFactor={0.01555}>
         <h2 className={`${styles.heading} ${styles.alignBottom}`}><Heading>Thank you</Heading></h2>
-      </Html>
-      <Html position={[-9, 0, -1]} className={styles.trippleCell}>
-        <p  style={{marginTop: 16}}>Let me thank you for the opportunity to be considered as a candidate for Improbable. I invite you to reach out to me. Let's explore how we can build unforgettable experiences together.</p>
+        <p  style={{marginTop: 32}}>Let me thank you for the opportunity to be considered as a candidate for Improbable. I invite you to reach out to me. Let's explore how we can build unforgettable experiences together.</p>
         <div className={styles.row}>
-          <div className={`${styles.arrowLeft} ${styles.inactive}`} style={{marginRight: 24}} onClick={() => setStage(4)}><span>Back</span></div>
+          <div className={`${styles.arrowLeft} ${styles.inactive}`} style={{marginRight: 24}} onClick={() => setStage(4, camera)}><span>Back</span></div>
           <a target='_blank' href="https://www.linkedin.com/in/sammael1106/" className={styles.socialLink}>
             <img src="images/linkedin-boerder-svgrepo-com.svg" width={32} height={32}/>
           </a>
